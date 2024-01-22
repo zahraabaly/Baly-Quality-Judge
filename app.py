64d213fbdc95af0c2199ef9f9b912668489272dd
Zahraa Jabar <zahraa.jabar@baly.iq>
2024-01-22 16:12:33 +0300

    handling empty data

--- app.py
+++ app.py
@@ -3,8 +3,6 @@ import pandas as pd
 from google.oauth2 import service_account
 import gspread
 from datetime import datetime
-import traceback
-# from google.auth.transport.requests import Request
 from google.oauth2.service_account import Credentials as ServiceAccountCredentials
 
 app = Flask(__name__)
@@ -23,9 +21,11 @@ worksheet = sh.worksheet("CC Cases")
 data_list = worksheet.get_all_records()
 df = pd.DataFrame(data_list)
 
+
 @app.route("/")
 def index():
     return render_template("index.html")
+
 
 @app.route("/search")
 def search():
@@ -45,18 +45,23 @@ def insert_data():
         city = data.get("city")
         service_type = data.get("serviceType")
 
+        print("Received Driver ID:", (driver_id))
+        print("Received reason:", (reason))
+        print("Received city:", (city))
+        print("Received service type:", (service_type))
+
         # Authorize and open the Google Sheet
         scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-        creds = ServiceAccountCredentials.from_json_keyfile_name("C:/Users/lenovo/Desktop/New project/baly-quality-judge-07fdacfafbc5.json", scope)
+        creds = ServiceAccountCredentials.from_json_keyfile_name("your-credentials.json", scope)
         client = gspread.authorize(creds)
-        
+
         # Open the Google Sheet by title
         spreadsheet_title = "All Quality Judge (New process)"
         sh = client.open(spreadsheet_title)
 
         # worksheet name is "CC Cases"
         sheet = sh.worksheet("CC Cases")
-        
+
         # Get the last empty row in column B
         next_row = len(sheet.col_values(2)) + 1
 
@@ -69,8 +74,6 @@ def insert_data():
 
         return jsonify({"status": "success"})
     except Exception as e:
-        print("Error during data insertion:", str(e))
-        traceback.print_exc()
         return jsonify({"status": "error", "message": str(e)})
 
 
